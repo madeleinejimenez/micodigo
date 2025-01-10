9c7651de2d6860b15c740550018065f02ac0972b
Madeleine Jimenez <majimenezgao@uide.edu.ec>
2025-01-10 08:46:45 -0500

    PUsh teacher

--- back/controllers/teacher.controller.js
+++ back/controllers/teacher.controller.js
@@ -5,6 +5,23 @@ const getList = async () => {
     return result;
 };
 
+const postCreate = async (data) => {
+    const result = await modelTeacher.create(data);
+
+    return result;
+};
+const postUpdate = async (data) => {
+    const result = await modelTeacher.update(data, {where: {id: data.id}});
+    return result;
+};
+const postDelete = async (data) => {
+    const result = await modelTeacher.destroy( {where: {id: data.id}});
+    return result;
+};
+
 module.exports = {
     getList,
+    postCreate,
+    postUpdate,
+    postDelete,
 };
--- back/routes/teacher.routes.js
+++ back/routes/teacher.routes.js
@@ -13,4 +13,28 @@ router.get("/list", function (response) {
   });
 });
 
+router.post("/create", function (request, response) {
+console.log(request.body);
+const result = teacherController.postCreate(request.body);
+response.status(200).json({
+  status: true,
+  info: result,
+  });  
+});
+router.post("/update", function (request, response) {
+  const result = teacherController.postUpdate(request.body);
+  response.status(200).json({
+    status: true,
+    info: result,
+    });  
+}); 
+
+router.post("/delete", function (request, response) {
+  const result = teacherController.postDelete(request.body);
+  response.status(200).json({
+    status: true,
+    info: result,
+    });  
+});
+
 module.exports = router;
